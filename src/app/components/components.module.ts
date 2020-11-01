26e056bccad126770641ea24136970f2d4984f11
AngieRoj <anlyrozy@gmail.com>
2020-11-01 12:13:46 -0500

    New component for the footer and changes in the styles

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DataCovid19Component } from './data-covid19/data-covid19.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, DataCovid19Component],
+  declarations: [NavbarComponent, DataCovid19Component, FooterComponent],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    DataCovid19Component
+    DataCovid19Component,
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
